826be2994cd27ab759aaa7c76f2b5e55beab9fa5
Göksel Pırnal <gokselpirnal@gmail.com>
2015-03-20 19:43:41 +0200

    run baby run

---
+++ Server.js
@@ -0,0 +1,28 @@
+﻿var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+	res.send("");
+});
+
+io.on('connection', function (socket) {
+	console.log("1 Yeni bağlantı.");
+	
+	socket.on('MesajGonder', function (data) {
+		socket.broadcast.emit('YeniMesajDinle',{'username':data.username,'message':data.message});
+		console.log(data.username +":"+data.message);
+	});
+		
+	socket.on('disconnect', function(){
+		console.log('1 Bağlantı sonlandı.');
+	});
+
+});
+
+server.listen(port);
+
+console.log('Uygulama ' + port + ' üzerinden ayakta');  
